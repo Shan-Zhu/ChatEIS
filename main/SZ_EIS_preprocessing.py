973ef2e84b5d967878aba95ae369e809e54ea715
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2023-07-24 09:18:52 +0800

    Update SZ_EIS_preprocessing.py

--- main/SZ_EIS_preprocessing.py
+++ main/SZ_EIS_preprocessing.py
@@ -1,88 +1,149 @@
+# coding: utf-8
 
-#coding: utf-8
-
-'''
-预处理：将原始csv文件中的f，Z,model等列单独提取
-'''
+'''Part of the code refers to impedance.py (doi = {10.21105/joss.02349}) and AutoECM (doi = {10.1149/1945-7111/acd8fb})'''
 
 import numpy as np
 import pandas as pd
-from scipy.interpolate import interp1d
-import matplotlib.pyplot as plt
 import json
+import os
+from tsfresh.transformers import RelevantFeatureAugmenter
+from tsfresh.transformers import FeatureAugmenter
+from tsfresh.feature_extraction import ComprehensiveFCParameters
 from sklearn.preprocessing import LabelEncoder
+from utils_preprocessing import preprocess_data
+from utils_preprocessing import unwrap_df
+from utils_preprocessing import exclude_outlier
+from utils_preprocessing import process_batch_element_params_str 
+from utils_preprocessing import process_batch_element_params
 
 
-def read_csv_data(file_path: str) -> pd.DataFrame:
-    df = pd.read_csv(file_path, index_col=0)
-    if "freq" in df.columns:
-        df["freq"] = df["freq"].apply(lambda x: np.array([float(c.strip("[]")) for c in x.split(", ")]))
-    if "Z" in df.columns:
-        df["Z"] = df["Z"].apply(lambda x: np.array([complex(c.strip("[]").replace(" ", "")) for c in x.split(", ")]))
-    return df
+def extract_tsfresh(df, df_test, output_path, le, if_exclude_outlier=False):
+    """Extracts the tsfresh features from the raw data and saves them to a file.
+    """
 
-def resize_array(arr, size):
-    n = arr.shape[0]
-    if size > n:
-        x_old = np.arange(n)
-        x_new = np.linspace(0, n-1, size)
-        f = interp1d(x_old, arr, kind='linear')
-        arr_resized = f(x_new)
-    elif size < n:
-        x_old = np.arange(n)
-        x_new = np.linspace(0, n-1, size)
-        f = interp1d(x_old, arr, kind='linear')
-        arr_resized = f(x_new)
-    else:
-        arr_resized = arr
-    return arr_resized
+    if if_exclude_outlier:
+        df, df_test = exclude_outlier(df, df_test)
+        output_path = output_path.replace(".", "ou.")
 
-def process_data_element(freq, Z, basis, func):
-    x = func(Z)
-    f = interp1d(freq, x, fill_value="extrapolate")
-    return f(basis)
+    # Resulting dataframes do not have labels! They only contain the id, freq, z_real, z_imag columns!
+    df_ts = unwrap_df(df)
+    df_ts_test = unwrap_df(df_test)
 
-def preprocess_data(file_path, size):
-    df = read_csv_data(file_path)
-    df["f"] = df.apply(lambda x: resize_array(x.freq, size), axis=1)
-    df["zreal"] = df.apply(lambda x: process_data_element(x.freq, x.Z, x.f, np.real), axis=1)
-    df["zimag"] = df.apply(lambda x: process_data_element(x.freq, x.Z, x.f, np.imag), axis=1)
-    return df
+    # Creating labels y
+    df_x = pd.DataFrame(index=np.unique(df_ts["id"]))
+    df_x_test = pd.DataFrame(index=np.unique(df_ts_test["id"]))
 
-def unwrap_dataframe(df):
-    df2 = pd.DataFrame(columns=["No.", "freq", "zreal", "zimag"])
-    for i in np.arange(df.shape[0]):
-        f, zreal, zimag = df[["f", "zreal", "zimag"]].loc[i]
-        No_x = np.tile(i, f.size)
-        df_ = pd.DataFrame(data=(No_x, f, zreal, zimag), index=["No.", "freq", "zreal", "zimag"]).T
-        df2 = pd.concat([df2, df_], ignore_index=True)
-    return df2
+    df_y = pd.Series(data=(le.transform(df["Circuit"])), index=np.unique(df_ts["id"]))
+    df_y_test = pd.Series(data=(le.transform(df_test["Circuit"])), index=np.unique(df_ts_test["id"]))
 
-def group_dataframe(df):
-    df_grouped = df.groupby("No.").apply(lambda x: pd.Series({"all": list(zip(x.freq, x.zreal, x.zimag))})).reset_index()
-    return df_grouped
+    augmenter = RelevantFeatureAugmenter(
+                    column_id="id",
+                    column_sort="freq",
+                    default_fc_parameters=ComprehensiveFCParameters(),
+                )
+    augmenter.set_params(timeseries_container=df_ts)
+    X = augmenter.fit_transform(df_x, df_y)
+    augmenter.set_params(timeseries_container=df_ts_test)
+    X_test = augmenter.transform(df_x_test)
 
-def tuples_to_string(tuples):
-    return ",".join([f"{d[0]:.9f},{d[1]:.9f},{d[2]:.9f}" for d in tuples])
+    # Save data
+    print("Saving tsfresh feature data...")
+    np.savetxt(output_path, np.c_[np.array(X), np.array(df_y)], delimiter=',')
+    np.savetxt(output_path.replace("train", "test"), np.c_[np.array(X_test), np.array(df_y_test)], delimiter=',')   
+    # Save the feature names as json 
+    with open(output_path.replace("train", "feature_names").replace('.csv', '.json'), 'w') as f:
+        json.dump(list(X.columns), f)
+        
+    return
 
-def rename_dataframe_columns(df):
-    df["all"] = df.apply(lambda x: tuples_to_string(x["all"]), axis=1)
-    df = df['all'].str.split(',', expand=True).add_prefix('A')
-    groups = [df.columns[i:i+3] for i in range(0, len(df.columns), 3)]
-    for i, group in enumerate(groups):
-        for j, col in enumerate(group):
-            new_col_name = f"{['freq', 'Zreal', 'Zimag'][j]}-{i+1}"
-            df = df.rename(columns={col: new_col_name})
-    return df
+def extract_raw_interpolated(df, df_test, output_path, le, if_exclude_outlier=False):
+    """Extracts the raw interpolated spectra from the raw data and saves them to a file."""
 
+    if if_exclude_outlier:
+        df, df_test = exclude_outlier(df, df_test)
+        output_path = output_path.replace(".csv", "_ourem.csv")
 
-csv_path="D:/ChatEIS/原始数据/AutoEIS/test_data.csv"
+    df_y = pd.Series(data=(le.transform(df["Circuit"])), index=df.index)
+    df_y_test = pd.Series(data=(le.transform(df_test["Circuit"])), index=df_test.index)
 
-eis_df= read_csv_data(csv_path)
-A=preprocess_data(csv_path, 20)
-B=unwrap_dataframe(A)
-C=group_dataframe(B)
-E=rename_dataframe_columns(C)
-E.to_csv('eis_test_data_20-test.csv', index=True)
+    # Exctract the spectra 
+    X_train = np.zeros((len(df), 2*len(df.zimag[0])))
+    X_test = np.zeros((len(df_test), 2*len(df_test.zimag[0])))
+    for i in range(len(df)):
+        X_train[i, :] = np.concatenate((df.zreal[i], -df.zimag[i]))
+    for i in range(len(df_test)):
+        X_test[i, :] = np.concatenate((df_test.zreal[i], -df_test.zimag[i]))
+        
+    data_train = np.concatenate((X_train, np.array(df_y).reshape(-1,1)), axis=1)
+    data_test = np.concatenate((X_test, np.array(df_y_test).reshape(-1,1)), axis=1)
 
-print('OK')
+    # Save data
+    print("Saving raw interpolated data...")
+    np.savetxt(output_path, data_train, delimiter=',')
+    np.savetxt(output_path.replace("train", "test"), data_test, delimiter=',')
+    print("Done.")    
+    return
+
+def plot_eis_nyquist_individual(df: pd.DataFrame, path, name='train', ):
+    """Plot the nyquist diagram for all eis data in the dataframe"""
+
+    nb_spectra = len(df)
+    # nb_spectra = 1 
+    for i in range(nb_spectra):
+        fig, ax  = plt.subplots(figsize = (2, 2), dpi=64, frameon=False)
+        ax.set_axis_off()
+        ax.plot(df['zreal'][i], -df['zimag'][i], linewidth=3, color='black')
+        plt.tight_layout()
+        try:
+            plt.savefig(f'{path}{name}/{df["Circuit"][i]}/{i}.png', dpi=64, bbox_inches='tight', pad_inches=0)
+        except:
+            os.makedirs(f'{path}{name}/{df["Circuit"][i]}')
+            plt.savefig(f'{path}{name}/{df["Circuit"][i]}/{i}.png', dpi=64, bbox_inches='tight', pad_inches=0)
+        plt.close()
+        if np.mod(i, 100) == 0:
+            print(f'Processed {i} spectra out of {nb_spectra}')
+    return
+
+def transform_data_to_images(df, df_test, le, output_path, plot_spectra=True):
+    """Transforms the raw data to images and saves them to a file."""
+
+    # Now let's make the images
+    plot_eis_nyquist_individual(df, output_path, name='train')
+    plot_eis_nyquist_individual(df_test, output_path, name='test')
+    return 
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    plt.ion()
+    train_data_fname = 'data/train_data.csv'
+    test_data_fname = 'data/test_data.csv'
+    output_path_tsfresh = 'data/train_tsfresh.csv'
+    output_path_raw = 'data/train_data_inter.csv'
+    output_path_cnn = 'data/images/'
+    le_path = 'data/le_name_mapping.json'
+
+    df = preprocess_data(train_data_fname)
+    df_test = preprocess_data(test_data_fname)
+
+    # Load label encoder
+    with open(le_path, 'r') as f:
+        mapping = json.load(f)
+        le = LabelEncoder()
+
+    nb_classes = len(mapping.keys())
+    mapping['classes'] = [mapping[str(int(i))] for i in range(nb_classes)]
+    le.classes_ = np.array(mapping['classes'])
+
+    # Creating labels y
+    with open(le_path, 'r') as f:
+        mapping = json.load(f)
+
+    le = LabelEncoder()
+    mapping['classes'] = [mapping[str(int(i))] for i in range(9)]
+    le.classes_ = np.array(mapping['classes'])
+    
+    # Extract features, by uncommenting the function you want to use
+    print("Preprocessing data...")
+    extract_raw_interpolated(df, df_test, output_path_raw, le, if_exclude_outlier=True)
+
+    print("Done.")
